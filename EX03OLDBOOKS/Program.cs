f1409adfbb11b3fea00a549e9fb90f37f72f7088
TeodorStnv <120647896+TeodorStnv@users.noreply.github.com>
2023-09-24 22:57:22 +0300

    Add project files.

---
+++ EX03OLDBOOKS/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace EX03OLDBOOKS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string favoriteBook = Console.ReadLine();
+            string nextBook = Console.ReadLine();
+            double numberOfBooks = 0;
+
+            while (nextBook != favoriteBook)
+            {
+               // numberOfBooks = numberOfBooks + 1;
+                
+                if (nextBook == "No More Books")
+                {
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine($"You checked {numberOfBooks} books.");
+                    break;
+                }
+
+                numberOfBooks = numberOfBooks + 1;
+                nextBook = Console.ReadLine();
+                   // numberOfBooks = numberOfBooks + 1;
+
+            }
+            if (nextBook==favoriteBook)
+            Console.WriteLine($"You checked {numberOfBooks} books and found it.");
+
+        }
+    }
+}
